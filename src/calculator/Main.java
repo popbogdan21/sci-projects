36cd6f623a4672ca0964a0763bbe8a0034d01652
popbogdan21 <41307339+popbogdan21@users.noreply.github.com>
2018-07-30 14:46:06 +0300

    function homework with comments

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -1,16 +1,21 @@
 package calculator;
 
-import calculator.Calculator;
-
-import java.awt.desktop.SystemEventListener;
-import java.util.Scanner;
-
+/* The program allows  user defined basic arithmetic functions
+    to be performed on two user defined numbers with user defined actions
+    Written by Pop Bogdan
+    Class Main has a single method main()
+ */
 public class Main{
 public static void main(String[]args){
         System.out.println("This program allows you to "+
         "perform basic operations between "+
         "two given numbers");
+
+        //Creates new class variable Calculator
         Calculator c= new Calculator();
+
+        //Calls the calcutaor method in the Calcutator class
         System.out.println("The result of the requested operation is: "+c.calculator());
+
         }
 }
